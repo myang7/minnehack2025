7e083c1f878c2c34d369e5f0998bfe6c5d3f9dce
Meiling <meiling.yang@tuta.io>
2025-02-08 23:17:38 -0600

    Add homepage and color scheming

--- website/main.js
+++ website/main.js
@@ -7,3 +7,9 @@ function navBarHide() {
     var x = document.getElementById("links");
     x.style.display = "none";
 }
+
+
+function showHome() {
+    var home = document.getElementsByClassName("title-page");
+    home.style.display = "block";
+}
